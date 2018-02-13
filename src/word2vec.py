66e5fb2f9839253d9903a4f3b0fea616f2aae236
wvanamstel <wvanamstel@quid.com>
2018-02-13 14:47:34 -0800

    remove old src

--- src/word2vec.py
+++
@@ -1,4 +0,0 @@
-from gensim.models import KeyedVectors
-
-# Load pretrained model (since intermediate data is not included, the model cannot be refined with additional data)
-model = KeyedVectors.load_word2vec_format('/opt/data/GoogleNews-vectors-negative300.bin', binary=True)
